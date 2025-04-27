c8e3b0f624d8f755b43b23dd5ccd754b84f8de35
Mohammad Rayhan <rayhanul.goni@upaybd.com>
2025-04-27 14:54:26 +0600

    feat: serializer config model created

--- dynamic_serializer/admin.py
+++ dynamic_serializer/admin.py
@@ -1,3 +1,11 @@
+# admin.py
 from django.contrib import admin
+from dynamic_serializer.models import SerializerConfig
 
-# Register your models here.
+
+@admin.register(SerializerConfig)
+class SerializerConfigAdmin(admin.ModelAdmin):
+    list_display = ('product_name', 'step', 'field_name', 'field_type', 'required', 'max_length', 'write_only', 'default_value', 'help_text', 'model_name')
+    search_fields = ('product_name', 'field_name', 'model_name')
+    list_filter = ('step', 'field_type', 'required', 'write_only', 'model_name')
+    ordering = ('product_name', 'step', 'field_name')
--- dynamic_serializer/models.py
+++ dynamic_serializer/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class SerializerConfig(models.Model):
+    product_name = models.CharField(max_length=255)
+    step = models.PositiveIntegerField()
+    field_name = models.CharField(max_length=100)
+    field_type = models.CharField(max_length=50)
+    required = models.BooleanField(default=True)
+    max_length = models.PositiveIntegerField(null=True, blank=True)
+    write_only = models.BooleanField(default=False)
+    default_value = models.CharField(max_length=255, null=True, blank=True)
+    help_text = models.CharField(max_length=255, null=True, blank=True)
+    regex_pattern = models.CharField(max_length=255, null=True, blank=True)
+    model_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"product: {self.product_name} - step: {self.step} - field: {self.field_name}"
